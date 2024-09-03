4aaec896214eae3c248524fda2413c8ec27f49c6
ryan <anch0rite.ryan@gmail.com>
2024-09-03 14:05:34 +0800

    load log4j configuration and initialize

--- src/core/server/Log.java
+++ src/core/server/Log.java
@@ -1,7 +1,14 @@
 package core.server;
 
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Log {
-	private static final String LOGGER_PROPERTIES_PATH = "./config/log4j.properties";
+	private static final String LOGGER_PROPERTIES_PATH = "./config/log4j2.xml";
 	private static Log instance;
 
 	public static Log getInstance () {
@@ -17,8 +24,16 @@ public class Log {
 	}
 
 	private Log () {
+		FileInputStream configFile;
 		try {
-			//TODO
+			configFile = new FileInputStream (LOGGER_PROPERTIES_PATH);
+
+			ConfigurationSource source = new ConfigurationSource (configFile);
+			Configurator.initialize (null, source);
+		} catch (FileNotFoundException e) {
+
+			//TODO: Create default configuration XML File
+
 		} catch (Exception e) {
 			e.printStackTrace ();
 			System.exit (-1);
